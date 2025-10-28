4671f76c88fc2ea88cb90e2462c0294f4d559ca1
Henrique Zambiazi <henriquez022009@gmail.com>
2025-10-28 01:03:15 +0000

    Tema de casa, primeiro tema feito readme.mdw

---
+++ tema.php
@@ -0,0 +1,39 @@
+<?php
+
+//TEMA 1
+
+$valor1 =1;
+$valor2 =2;
+$valor3 =3;
+$valor4 =4;
+$valor5 =5;
+$valor6 =6;
+$valor7 =7;
+$valor8 =8;
+$valor9 =9;
+$valor10 =10;
+
+ECHO $multiplicação = $valor1 * $valor5;
+ECHO $multiplicação = $valor2 * $valor5;
+ECHO $multiplicação = $valor3 * $valor5;
+ECHO $multiplicação = $valor4 * $valor5;
+ECHO $multiplicação = $valor5 * $valor5;
+ECHO $multiplicação = $valor6 * $valor5;
+ECHO $multiplicação = $valor7 * $valor5;
+ECHO $multiplicação = $valor8 * $valor5;
+ECHO $multiplicação = $valor9 * $valor5;
+ECHO $multiplicação = $valor10 * $valor5;
+
+
+//ECHO $multiplicação = $valor1 * $valor5 "<br>";
+//ECHO $multiplicação = $valor2 * $valor5 "<br>";
+//ECHO $multiplicação = $valor3 * $valor5 "<br>";
+//ECHO $multiplicação = $valor4 * $valor5 "<br>";
+//ECHO $multiplicação = $valor5 * $valor5 "<br>";
+//ECHO $multiplicação = $valor6 * $valor5 "<br>";
+//ECHO $multiplicação = $valor7 * $valor5 "<br>";
+//ECHO $multiplicação = $valor8 * $valor5 "<br>";
+//ECHO $multiplicação = $valor9 * $valor5 "<br>";
+//ECHO $multiplicação = $valor10 * $valor5 "<br>";
+
+
