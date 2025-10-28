6c901e08daf8640b9fb57ec17346d4e9d2793528
Henrique Zambiazi <henriquez022009@gmail.com>
2025-10-28 02:16:54 +0000

    Conclusão de tema readme.mdw

--- tema.php
+++ tema.php
@@ -62,3 +62,40 @@ ECHO $multiplicação = $valor10 * $valor6;
 
 //Tema 2
 
+//Aritimética
+$Nota1 = 12;
+$Nota2 = 8;
+$Nota3 = 4;
+$Nota4 = 24;
+$Nota5 = 12;
+
+$soma1 = ($Nota1 + $Nota2 + $Nota3 + $Nota4 + $Nota5) / 5;
+//ECHO "<br>"
+ECHO "A média aritimética das notas é $soma1";
+
+//Ponderada
+$V1 = 10;
+$V2 = 12;
+$V3 = 6;
+$V4 = 3;
+$V5 = 21;
+
+$P1 = 12;
+$P2 = 12;
+$P3 = 8;
+$P4 = 4;
+$P5 = 24;
+
+$soma2 = (($V1 * $P1) + ($V2 * $P2) + ($V3 * $P3) + ($V4 * $P4) + ($V5 * $P5)) / ($P1 + $P2 + $P3 + $P4 + $P5);
+ECHO "A Média Ponderada do meu semestre escolar foi de $soma2";
+
+
+//Harmonica
+$KM1 = 7;
+$KM2 = 3;
+$KM3 = 4;
+$KM4 = 9;
+$KM5 = 2;
+$KMT = 5;
+$soma3 = $KMT / (( 1 / $KM1 ) + ( 1 / $KM2 ) + ( 1 / $KM3 ) + ( 1 / $KM4 ) + (1 / $KM5))
+ECHO "A Média Harmonica da minha viagem é $soma3" 
