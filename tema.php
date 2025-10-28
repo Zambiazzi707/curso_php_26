1f1c739cff5bcd811055c17a04da0396904dab45
Henrique Zambiazi <henriquez022009@gmail.com>
2025-10-28 01:11:35 +0000

    Segunda tabuada (6) feita readme.mdw

--- tema.php
+++ tema.php
@@ -36,4 +36,29 @@ ECHO $multiplicação = $valor10 * $valor5;
 //ECHO $multiplicação = $valor9 * $valor5 "<br>";
 //ECHO $multiplicação = $valor10 * $valor5 "<br>";
 
+ECHO $multiplicação = $valor1 * $valor6;
+ECHO $multiplicação = $valor2 * $valor6;
+ECHO $multiplicação = $valor3 * $valor6;
+ECHO $multiplicação = $valor4 * $valor6;
+ECHO $multiplicação = $valor5 * $valor6;
+ECHO $multiplicação = $valor6 * $valor6;
+ECHO $multiplicação = $valor7 * $valor6;
+ECHO $multiplicação = $valor8 * $valor6;
+ECHO $multiplicação = $valor9 * $valor6;
+ECHO $multiplicação = $valor10 * $valor6;
+
+
+//ECHO $multiplicação = $valor1 * $valor6 "<br>";
+//ECHO $multiplicação = $valor2 * $valor6 "<br>";
+//ECHO $multiplicação = $valor3 * $valor6 "<br>";
+//ECHO $multiplicação = $valor4 * $valor6 "<br>";
+//ECHO $multiplicação = $valor5 * $valor6 "<br>";
+//ECHO $multiplicação = $valor6 * $valor6 "<br>";
+//ECHO $multiplicação = $valor7 * $valor6 "<br>";
+//ECHO $multiplicação = $valor8 * $valor6 "<br>";
+//ECHO $multiplicação = $valor9 * $valor6 "<br>";
+//ECHO $multiplicação = $valor10 * $valor6 "<br>";
+
+
+//Tema 2
 
