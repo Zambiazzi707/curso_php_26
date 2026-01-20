183c661e33f07851cd77438324976cdb6d0f940d
zambiazihenrique <henriquez022009@gmail.com>
2026-01-20 11:32:33 -0300

    Correção Aulas passadas

--- curso_php_26/Aula_13_01_26_SalaDeAula.php
+++ curso_php_26/Aula_13_01_26_SalaDeAula.php
@@ -1,55 +1,72 @@
-<?php 
+<?php
+
+define("QUEBRAR_LINHA", "<br>");
 
 class SalaDeAula {
     public $numero = 0;
     public $classes = [];
-    public $luzLigada = false;
-    public $lampadaQueimada = false;
+    public $luzLigada;
+    public $lampadaQueimada;
     public $arCondicionado = "";
 
-    public function __construct($numero) {
-
+    public function __construct($numero)
+    {
         $this->numero = $numero;
-
+        $this->luzLigada = false;
+        $this->lampadaQueimada = false;
     }
-    public function ligarALuz(){
-        if ($this->verificarALampada); 
 
+    public function ligarALuz() {
+        $estaQueimada = $this->verificarLampada();
+
+        if ($estaQueimada) {
+             return "A sala numero:{$this->numero} está com a lampada queimada." . QUEBRAR_LINHA;
+        }
+
+        $this->luzLigada = true;
+
+         return "A sala numero:{$this->numero} está com a luz ligada." . QUEBRAR_LINHA;
     }
 
     public function desligarALuz() {
-        $estaQueimada = $this->verificararALampada;
+        
+        $this->luzLigada = false;
+
+         return "A sala numero:{$this->numero} está com a luz desligada." . QUEBRAR_LINHA;
     }
 
+    public function verificarLampada() {
+        $estaQueimada =  $this->lampadaQueimada;
 
-    public function verificarALâmpada () {
-        $estaQueimada = $this->lampadaQueimada;
-            
         if ($estaQueimada) {
-            return true;
+           return true;
         }
 
-            return false;
-
+        return false;
     }
 
-        
-    public function queimarALâmpada () {
+    public function queimarLampada() {
         $this->lampadaQueimada = true;
         $this->luzLigada = false;
     }
 
-        
-    public function trocarALâmpada () {
+    public function trocarLampada() {
 
+        $estaLigada = $this->luzLigada;
+
+        if ($estaLigada) {
+            $this->luzLigada = false;
+        }
+
+        $this->luzLigada = false;
+        $this->lampadaQueimada = false;
     }
-        
 }
 
+$numero = 10;
 $objSalaDeAula = new SalaDeAula(101);
-
 echo $objSalaDeAula->ligarALuz();
-$objSalaDeAula->queimarALampada();
+$objSalaDeAula->queimarLampada();
 echo $objSalaDeAula->ligarALuz();
-$objSalaDeAula->trocarALampada();
+$objSalaDeAula->trocarLampada();
 echo $objSalaDeAula->ligarALuz();
